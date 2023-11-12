d4b25d3c97aa15505817a465c180c302afa20004
Evgeniy <evgeni.moskvin@gmail.com>
2023-11-12 19:50:44 +0300

    upd settings.py

--- start_page_app/models.py
+++ start_page_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from os import path
+
+
+def upload_to(instance, filename):
+    name_to_path = str(instance.id)
+    print(instance.id)
+    new_path = path.join('files',
+                         # "media", filename)
+                         name_to_path,
+                         filename)
+    print(new_path)
+    return new_path
 
 # Create your models here.
+class LinksModel(models.Model):
+    number = models.IntegerField(verbose_name="Порядковый номер", null=True, blank=True, unique=True)
+    name = models.CharField(verbose_name="Название", null=True, blank=True, max_length=15)
+    image = models.ImageField(verbose_name="Изображение", null=True, blank=True,
+                              upload_to=upload_to)
+    link = models.URLField(verbose_name="Ссылка")
+
+    class Meta:
+        verbose_name = _("ссылка")
+        verbose_name_plural = _("ссылки")
+
+    def __str__(self):
+        return f'{self.number} - {self.name}'
+
--- start_page_app/views.py
+++ start_page_app/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, redirect
-from django.utils import timezone
-from django.contrib.auth.decorators import login_required
 from django.views import View
 
-from django.utils.decorators import method_decorator
-from django.utils.encoding import escape_uri_path
-from django.conf import settings
-from django.contrib.auth import authenticate, login
+from .models import LinksModel
 
 
 class IndexMainPage(View):
+    """
+    Главная страница
+    """
+
     def get(self, request):
-        return render(request, 'start_page_app/index.html')
+        # Получаем ссылки из базы, сортируем по номеру порядковому
+        links = LinksModel.objects.get_queryset().order_by('number')
+        count_none = 9 - links.count()  # Получаем количество пустых ячеек
+        none_list = [i for i in range(count_none)]  # Генерируем список для цикла на фронте
+        content = {
+            'links': links,
+            'none_list': none_list,
+        }
+        return render(request, 'start_page_app/index.html', content)
